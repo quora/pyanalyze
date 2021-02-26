a7849720442429df202b04c2956e5f67cc758f18
Jelle Zijlstra <jelle@quora.com>
2021-02-26 02:39:13 +0000

    2.7 strikes again

--- pyanalyze/test_name_check_visitor.py
+++ pyanalyze/test_name_check_visitor.py
@@ -468,20 +468,24 @@ def capybara():
         def tucotuco():
             assert_is_value(2 + 3, KnownValue(5))
 
-    @assert_passes()
+    @skip_before((3, 0))
     def test_inplace_binop(self):
-        class Capybara:
-            def __add__(self, x: int) -> str:
-                return ""
+        self.assert_passes(
+            """
+class Capybara:
+    def __add__(self, x: int) -> str:
+        return ""
 
-            def __iadd__(self, x: str) -> int:
-                return 0
+    def __iadd__(self, x: str) -> int:
+        return 0
 
-        def tucotuco():
-            x = Capybara()
-            assert_is_value(x + 1, TypedValue(str))
-            x += "a"
-            assert_is_value(x, TypedValue(int))
+def tucotuco():
+    x = Capybara()
+    assert_is_value(x + 1, TypedValue(str))
+    x += "a"
+    assert_is_value(x, TypedValue(int))
+"""
+        )
 
     @assert_passes()
     def test_global_sets_value(self):
