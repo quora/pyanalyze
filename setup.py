f67909270d95dc231d5055cb2ac77cc0249ac0cb
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-05-17 03:25:57 -0700

    version 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 
 
-VERSION = "0.2.0rc1"
+VERSION = "0.2.0"
 # Used in internal packaging system.
 if "SANTA_PACKAGE_VERSION" in os.environ:
     VERSION = "%s.%s" % (VERSION, os.environ["SANTA_PACKAGE_VERSION"])
