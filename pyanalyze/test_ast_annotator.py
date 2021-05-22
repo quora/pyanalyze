ba6ef7e294d9c7d81f500b8e919e5a0419abac8f
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-05-21 18:54:03 -0700

    fix annotator test case (#201)

--- pyanalyze/test_ast_annotator.py
+++ pyanalyze/test_ast_annotator.py
@@ -1,6 +1,6 @@
 import ast
 from pathlib import Path
-from typing import Type
+from typing import Callable, Type
 from qcore.asserts import assert_eq
 
 from .ast_annotator import annotate_file, annotate_code
@@ -9,10 +9,13 @@ from .value import KnownValue, Value
 
 
 def _check_inferred_value(
-    tree: ast.Module, node_type: Type[ast.AST], value: Value
+    tree: ast.Module,
+    node_type: Type[ast.AST],
+    value: Value,
+    predicate: Callable[[ast.AST], bool] = lambda node: True,
 ) -> None:
     for node in ast.walk(tree):
-        if isinstance(node, node_type):
+        if isinstance(node, node_type) and predicate(node):
             assert hasattr(node, "inferred_value"), repr(node)
             assert_eq(value, node.inferred_value, extra=ast.dump(node))
 
@@ -41,6 +44,22 @@ x.a + 1
     """
     )
     _check_inferred_value(tree, ast.BinOp, KnownValue(2))
+
+    tree = annotate_code(
+        """
+class A:
+    def __init__(self):
+        self.a = 1
+
+    def bla(self):
+        return self.a
+
+
+a = A()
+b = a.bla()
+"""
+    )
+    _check_inferred_value(tree, ast.Name, KnownValue(1), lambda node: node.id == "b")
 
 
 def test_everything_annotated() -> None:
