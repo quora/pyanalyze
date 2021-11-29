d88cc70bd857655e23b9ba374fc3e0033609adf0
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-11-29 14:05:20 -0800

    we do not have a _static

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
