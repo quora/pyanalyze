758ef5d474a7791474a3cfb36d54dc965b2d40ba
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-11-29 13:55:18 -0800

    improve docs build (mostly stolen from black)

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from pkg_resources import get_distribution
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -23,7 +24,7 @@ copyright = "2021, Jelle Zijlstra"
 author = "Jelle Zijlstra"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = get_distribution("pyanalyze").version
 
 
 # -- General configuration ---------------------------------------------------
@@ -56,3 +57,7 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = [".rst", ".md"]
