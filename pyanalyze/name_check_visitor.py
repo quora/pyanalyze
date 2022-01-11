2e5402654cfd717b3fe7818ef8af06a7785eaded
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-10 16:48:18 -0800

    put back ignore_none for attributes

--- pyanalyze/name_check_visitor.py
+++ pyanalyze/name_check_visitor.py
@@ -3962,7 +3962,11 @@ class NameCheckVisitor(node_visitor.ReplacingNodeVisitor):
                         root_composite.value.val.__name__, node.attr, node, self
                     )
             value = self.get_attribute(
-                root_composite, node.attr, node, use_fallback=True
+                root_composite,
+                node.attr,
+                node,
+                use_fallback=True,
+                ignore_none=self.options.get_value_for(IgnoreNoneAttributes),
             )
             if self._should_use_varname_value(value):
                 varname_value = self.checker.maybe_get_variable_name_value(node.attr)
