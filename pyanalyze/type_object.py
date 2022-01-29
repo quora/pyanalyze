8ddd1394ced77a6fc2ee6729f2fc2fe900cf670e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-28 18:42:20 -0800

    not that part

--- pyanalyze/type_object.py
+++ pyanalyze/type_object.py
@@ -134,9 +134,7 @@ class TypeObject:
             with ctx.assume_compatibility(self, other):
                 tv_maps = []
                 for member in self.protocol_members:
-                    expected = ctx.get_attribute_from_value(
-                        self_val, member, prefer_typeshed=True
-                    )
+                    expected = ctx.get_attribute_from_value(self_val, member)
                     # For __call__, we check compatiiblity with the other object itself.
                     if member == "__call__":
                         actual = other_val
