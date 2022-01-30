667d40a9e5985210d403d8c965a76b9774b5dd48
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-29 18:16:58 -0800

    add test_typevar.py (#452)

--- pyanalyze/test_signature.py
+++ pyanalyze/test_signature.py
@@ -1,5 +1,4 @@
 # static analysis: ignore
-from pyanalyze.implementation import assert_is_value
 from collections.abc import Sequence
 
 from .value import (
@@ -9,11 +8,11 @@ from .value import (
     CanAssignError,
     GenericValue,
     KnownValue,
-    MultiValuedValue,
     SequenceIncompleteValue,
     TypedDictValue,
     TypedValue,
 )
+from .implementation import assert_is_value
 from .test_name_check_visitor import TestNameCheckVisitorBase
 from .test_node_visitor import assert_fails, assert_passes, skip_before
 from .error_code import ErrorCode
@@ -858,160 +857,6 @@ class TestCalls(TestNameCheckVisitorBase):
             kwonly(a=1, b="")  # ok
             kwonly(**td)  # ok
             kwonly(**bad_td)  # E: incompatible_call
-
-
-class TestTypeVar(TestNameCheckVisitorBase):
-    @assert_passes()
-    def test_simple(self):
-        from typing import TypeVar, List, Generic
-
-        T = TypeVar("T")
-
-        def id(obj: T) -> T:
-            return obj
-
-        def get_one(obj: List[T]) -> T:
-            for elt in obj:
-                return elt
-            assert False
-
-        class GenCls(Generic[T]):
-            def get_one(self: "GenCls[T]") -> T:
-                raise NotImplementedError
-
-            def get_another(self) -> T:
-                raise NotImplementedError
-
-        def capybara(x: str, xs: List[int], gen: GenCls[int]) -> None:
-            assert_is_value(id(3), KnownValue(3))
-            assert_is_value(id(x), TypedValue(str))
-            assert_is_value(get_one(xs), TypedValue(int))
-            assert_is_value(get_one([int(3)]), TypedValue(int))
-            # This one doesn't work yet because we don't know how to go from
-            # KnownValue([3]) to a GenericValue of some sort.
-            # assert_is_value(get_one([3]), KnownValue(3))
-
-            assert_is_value(gen.get_one(), TypedValue(int))
-            assert_is_value(gen.get_another(), TypedValue(int))
-
-    @assert_passes()
-    def test_union_math(self):
-        from typing import TypeVar, Optional
-
-        T = TypeVar("T")
-
-        def assert_not_none(arg: Optional[T]) -> T:
-            assert arg is not None
-            return arg
-
-        def capybara(x: Optional[int]):
-            assert_is_value(x, MultiValuedValue([KnownValue(None), TypedValue(int)]))
-            assert_is_value(assert_not_none(x), TypedValue(int))
-
-    @assert_passes()
-    def test_only_T(self):
-        from typing import Generic, TypeVar
-
-        T = TypeVar("T")
-
-        class Capybara(Generic[T]):
-            def add_one(self, obj: T) -> None:
-                pass
-
-        def capybara(x: Capybara[int]) -> None:
-            x.add_one("x")  # E: incompatible_argument
-
-    @assert_passes()
-    def test_multi_typevar(self):
-        from typing import TypeVar, Optional
-
-        T = TypeVar("T")
-
-        # inspired by tempfile.mktemp
-        def mktemp(prefix: Optional[T] = None, suffix: Optional[T] = None) -> T:
-            raise NotImplementedError
-
-        def capybara() -> None:
-            assert_is_value(mktemp(), AnyValue(AnySource.generic_argument))
-            assert_is_value(mktemp(prefix="p"), KnownValue("p"))
-            assert_is_value(mktemp(suffix="s"), KnownValue("s"))
-            assert_is_value(mktemp("p", "s"), KnownValue("p") | KnownValue("s"))
-
-    @assert_passes()
-    def test_generic_base(self):
-        from typing import TypeVar, Generic
-
-        T = TypeVar("T")
-
-        class Base(Generic[T]):
-            pass
-
-        class Derived(Base[int]):
-            pass
-
-        def take_base(b: Base[int]) -> None:
-            pass
-
-        def capybara(c: Derived):
-            take_base(c)
-
-    @assert_fails(ErrorCode.incompatible_argument)
-    def test_wrong_generic_base(self):
-        from typing import TypeVar, Generic
-
-        T = TypeVar("T")
-
-        class Base(Generic[T]):
-            pass
-
-        class Derived(Base[int]):
-            pass
-
-        def take_base(b: Base[str]) -> None:
-            pass
-
-        def capybara(c: Derived):
-            take_base(c)
-
-    @skip_before((3, 10))
-    @assert_passes()
-    def test_typeshed(self):
-        from typing import List
-
-        def capybara(lst: List[int]) -> None:
-            lst.append("x")  # E: incompatible_argument
-
-    @assert_passes()
-    def test_generic_super(self):
-        from typing import Generic, TypeVar
-
-        T = TypeVar("T")
-
-        class A(Generic[T]):
-            def capybara(self) -> None:
-                pass
-
-        class B(A):
-            def capybara(self) -> None:
-                super().capybara()
-
-    @assert_passes()
-    def test_default(self):
-        from typing import TypeVar, Dict, Union
-
-        KT = TypeVar("KT")
-        VT = TypeVar("VT")
-        T = TypeVar("T")
-
-        def dictget(d: Dict[KT, VT], key: KT, default: T = None) -> Union[VT, T]:
-            try:
-                return d[key]
-            except KeyError:
-                return default
-
-        def capybara(d: Dict[str, str], key: str) -> None:
-            assert_is_value(dictget(d, key), TypedValue(str) | KnownValue(None))
-            assert_is_value(dictget(d, key, 1), TypedValue(str) | KnownValue(1))
 
 
 class TestAllowCall(TestNameCheckVisitorBase):
