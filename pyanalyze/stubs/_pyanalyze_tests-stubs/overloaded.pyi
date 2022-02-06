a8295907f0e16e64e85e862bd1c8a95efbb4ee1e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-02-05 20:45:37 -0800

    add stub

---
+++ pyanalyze/stubs/_pyanalyze_tests-stubs/overloaded.pyi
@@ -0,0 +1,9 @@
+from typing import overload
+from typing_extensions import Literal
+
+@overload
+def func(x: int, y: Literal[1] = ..., z: str = ...) -> int: ...
+@overload
+def func(x: int, y: int, z: str = ...) -> str: ...
+@overload
+def func(x: str, y: int = ..., z: str = ...) -> float: ...
