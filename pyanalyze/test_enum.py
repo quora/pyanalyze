a254759edd721e049fc7bc39a704a2c6a20c86cf
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-03-02 14:14:30 -0800

    Infer callable signatures for objects with a `__getattr__` method

---
+++ pyanalyze/test_enum.py
@@ -0,0 +1,29 @@
+# static analysis: ignore
+from .implementation import assert_is_value
+from .value import SubclassValue, TypedValue
+from .test_name_check_visitor import TestNameCheckVisitorBase
+from .test_node_visitor import assert_passes
+
+
+class TestEnum(TestNameCheckVisitorBase):
+    @assert_passes()
+    def test_functional(self):
+        from enum import Enum
+
+        def capybara():
+            X = Enum("X", ["a", "b", "c"])
+            assert_is_value(X, SubclassValue(TypedValue(Enum)))
+
+    @assert_passes()
+    def test_call(self):
+        from enum import Enum
+
+        class X(Enum):
+            a = 1
+            b = 2
+
+        def capybara():
+            assert_is_value(X(1), TypedValue(X))
+            # This should be an error, but the typeshed
+            # stubs are too lenient.
+            assert_is_value(X(None), TypedValue(X))
