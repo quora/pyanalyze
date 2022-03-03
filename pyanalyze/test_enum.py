e3f90c39846e2f0e9c47003cfd856b6d88517e3e
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-03-02 22:15:57 -0800

    fix for Enum subclasses (#488)

--- pyanalyze/test_enum.py
+++ pyanalyze/test_enum.py
@@ -8,11 +8,14 @@ from .test_node_visitor import assert_passes
 class TestEnum(TestNameCheckVisitorBase):
     @assert_passes()
     def test_functional(self):
-        from enum import Enum
+        from enum import Enum, IntEnum
 
         def capybara():
             X = Enum("X", ["a", "b", "c"])
             assert_is_value(X, SubclassValue(TypedValue(Enum)))
+
+            IE = IntEnum("X", ["a", "b", "c"])
+            assert_is_value(IE, SubclassValue(TypedValue(Enum)))
 
     @assert_passes()
     def test_call(self):
