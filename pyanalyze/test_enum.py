b31ca3f34839814b495e7c6b094e74564fd80017
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-03-03 10:49:21 -0800

    more tests for enum iteration

--- pyanalyze/test_enum.py
+++ pyanalyze/test_enum.py
@@ -30,3 +30,22 @@ class TestEnum(TestNameCheckVisitorBase):
             # This should be an error, but the typeshed
             # stubs are too lenient.
             assert_is_value(X(None), TypedValue(X))
+
+    @assert_passes()
+    def test_iteration(self):
+        from enum import Enum, IntEnum
+        from typing import Type
+
+        class X(Enum):
+            a = 1
+            b = 2
+
+        def capybara(enum_t: Type[Enum], int_enum_t: Type[IntEnum]):
+            for x in X:
+                assert_is_value(x, TypedValue(X))
+
+            for et in enum_t:
+                assert_is_value(et, TypedValue(Enum))
+
+            for iet in int_enum_t:
+                assert_is_value(iet, TypedValue(IntEnum))
