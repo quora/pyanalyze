b492ce4f25cb332854a53372d9220593585f289d
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-04-11 18:36:37 -0700

    remove unused imports

--- pyanalyze/test_asynq.py
+++ pyanalyze/test_asynq.py
@@ -6,9 +6,7 @@ from .value import (
     AnyValue,
     AsyncTaskIncompleteValue,
     KnownValue,
-    MultiValuedValue,
     TypedValue,
-    GenericValue,
     DictIncompleteValue,
     KVPair,
 )
