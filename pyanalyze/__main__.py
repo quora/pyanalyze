57fe25cb6c43a54a26a57ff0be9b03dbe00f8ae5
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-15 05:47:22 -0400

    Update __main__.py

--- pyanalyze/__main__.py
+++ pyanalyze/__main__.py
@@ -1,5 +1,6 @@
 import sys
 from os import system
+
 from pyanalyze.name_check_visitor import NameCheckVisitor
 
 # Enable ANSI color codes for Windows cmd using this strange workaround ( see https://github.com/python/cpython/issues/74261 )
