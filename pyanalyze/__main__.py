efdb2f5070b4669975bfee6bc301c594cdcc28c6
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-15 05:51:21 -0400

    Update __main__.py: Line too long

--- pyanalyze/__main__.py
+++ pyanalyze/__main__.py
@@ -3,7 +3,8 @@ from os import system
 
 from pyanalyze.name_check_visitor import NameCheckVisitor
 
-# Enable ANSI color codes for Windows cmd using this strange workaround ( see https://github.com/python/cpython/issues/74261 )
+# Enable ANSI color codes for Windows cmd using this strange workaround
+# ( see https://github.com/python/cpython/issues/74261 )
 system("")
 
 
