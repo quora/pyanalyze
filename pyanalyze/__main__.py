6b931cd431410007d8385250d5b655e2ccfc4892
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-15 05:38:41 -0400

    Update __main__.py: manually "apply black"

--- pyanalyze/__main__.py
+++ pyanalyze/__main__.py
@@ -1,8 +1,9 @@
 import sys
 from os import system
-system("") # Enable ANSI color codes for Windows cmd using this strange workaround ( see https://github.com/python/cpython/issues/74261 )
-
 from pyanalyze.name_check_visitor import NameCheckVisitor
+
+# Enable ANSI color codes for Windows cmd using this strange workaround ( see https://github.com/python/cpython/issues/74261 )
+system("")
 
 
 def main() -> None:
