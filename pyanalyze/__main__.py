a71628a39db6cc33626b4645de0a710da5318f52
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-16 02:18:39 -0400

    Update __main__.py: do this only on Windows to avoid doing unnecessary work on other OSes, and let's do it inside the main() function so this doesn't get run if the file is merely imported

--- pyanalyze/__main__.py
+++ pyanalyze/__main__.py
@@ -1,14 +1,14 @@
 import sys
-from os import system
+import os
 
 from pyanalyze.name_check_visitor import NameCheckVisitor
 
-# Enable ANSI color codes for Windows cmd using this strange workaround
-# ( see https://github.com/python/cpython/issues/74261 )
-system("")
-
 
 def main() -> None:
+    if os.name == "nt":
+        # Enable ANSI color codes for Windows cmd using this strange workaround
+        # ( see https://github.com/python/cpython/issues/74261 )
+        os.system("")
     sys.exit(NameCheckVisitor.main())
 
 
